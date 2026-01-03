95cbccad08569448db4ae9c2d0cf6b595e857fb0
tin-auppati <tin.auppati@gmail.com>
2026-01-03 17:03:45 +0700

    Feat: Connect frontend to display Text CAPTCHA from backend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,65 +1,75 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
-}
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Home(){
+    //สร้าง state เอาไว้เก็บค่าว่าง
+    // imageURL เริ่มต้นเป็นค่าว่าง ""
+    const [imageURL, setImageURL] = useState("")
+    const [loading, setLoading] = useState(false);
+
+
+    //ขโมยรูปจาก backend
+    const fetchCaptcha = async() => {
+        try{
+            setLoading(true); //หมุน
+            //ยิงไปที่ go backend
+            const res = await axios.get("http://localhost:8080/api/captcha")
+            
+            //เอา base 64 ใส่ลงใน imageurl
+            setImageURL(res.data.image)
+        } catch(error) {
+            console.error("Error fetching captcha:", error);
+            alert("เชื่อมต่อ backend ไม่สำเร็จ");
+        } finally {
+            setLoading(false)
+        }
+
+    };
+
+    useEffect(()=> {
+        fetchCaptcha();
+    },[]);
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-clear bg-gray-50 p-6">
+            <div className="max-w-md w-full bg-white rounded-xl shadow-lg overflow-hidden p-8">
+                {/* หัวข้อ */}
+                <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
+                    Security Friction Research
+                </h1>
+
+                <div className="flex flex-col items-center gap-6">
+                    {/* กล่องแสดงรูป CAPTCHA */}
+                    <div className="boarder-2 boarder-gray-200 rounded-lg p-4 bg-gray-100 flex items-center justify-center w-full min-h-[100px]">
+                        {loading ? (
+                            <span className="text-gray-200 animate-pulse">Loading...</span>
+                        ) : imageURL ? (
+                            //เอา base64 มาแสดงเป็นรูป
+                            <img src={imageURL} alt="CAPTCHA" className="h-16 object-contain" />
+                        ) : (
+                            <span className="text-red-400">No Image</span>
+                        )}
+                    </div>
+
+                    {/* ช่องกรอกข้อมูล (ยังไม่ได้ทำระบบตรวจ) */}
+                    <input 
+                        type="text"
+                        placeholder="Type charactors above"
+                        className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none text-center text-lg tracking-widest uppercase text-black"
+                        />
+                    {/* ปุ่มรีเฟรช */}
+
+                    <button
+                        onClick={fetchCaptcha}
+                        className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-200 flex items-center justify-center gap-2"
+                        >
+                    New Challenge
+                    </button>
+                </div>
+            </div>
+        </main>
+    )
+
+};
