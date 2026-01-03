21c4b835b4fc118c66d9f3c5e01143e98ec8d8ea
tin-auppati <tin.auppati@gmail.com>
2026-01-03 22:56:02 +0700

    Fix: frontend Hot reload

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,             // เช็คไฟล์ทุกๆ 1 วินาที
+      aggregateTimeout: 300,  // รอ 0.3 วิให้ save เสร็จจริงค่อยรัน
+    }
+    return config
+  },
 };
 
 export default nextConfig;
